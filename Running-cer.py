5be880350807b17fe17f757318a4157c3cdb3feb
DengShicong <144335768+DengShicong@users.noreply.github.com>
2023-11-21 10:20:50 +0800

    Update Running-cer.py

--- Running-cer.py
+++ Running-cer.py
@@ -3,6 +3,21 @@ import pandas as pd
 from datetime import datetime
 from netmiko import ConnectHandler
 import re
+import multiprocessing
+
+def worker(device_info, output_queue):
+    try:
+        with ConnectHandler(**device_info) as net_connect:
+            net_connect.enable()
+            output = net_connect.send_command('show running-directory')
+            running_config_lines = re.findall(r'Running configuration.*(?:\n(?!\n).*)*', output)
+            if running_config_lines:
+                formatted_output = "\n".join(running_config_lines)
+                formatted_output = re.sub(r"WORKING", r"<span style='color:green;'>WORKING</span>", formatted_output)
+                formatted_output = re.sub(r"CERTIFIED", r"<span style='color:red;'>CERTIFIED</span>", formatted_output)
+                output_queue.put(f"{device_info['host']}:\n{formatted_output}\n\n")
+    except Exception as e:
+        output_queue.put(f"Connection failed for {device_info['host']}: {e}")
 
 def execute_commands_and_save_to_html(template_path):
     template_data = pd.ExcelFile(template_path)
@@ -13,7 +28,10 @@ def execute_commands_and_save_to_html(template_path):
 
     running_directory_outputs = "<html><body><pre>"
 
-    for index, row in assets_data.iterrows():
+    output_queue = multiprocessing.Queue()
+    processes = []
+
+    for _, row in assets_data.iterrows():
         device_info = {
             "device_type": row["device_type"],
             "host": row["IP"],
@@ -22,22 +40,15 @@ def execute_commands_and_save_to_html(template_path):
             "port": int(row["port"]) if pd.notna(row["port"]) else 22,
             "secret": row["secret"]
         }
+        p = multiprocessing.Process(target=worker, args=(device_info, output_queue))
+        processes.append(p)
+        p.start()
 
-        try:
-            with ConnectHandler(**device_info) as net_connect:
-                net_connect.enable()
+    for p in processes:
+        p.join()
 
-                output = net_connect.send_command('show running-directory')
-
-                running_config_lines = re.findall(r'Running configuration.*(?:\n(?!\n).*)*', output)
-                if running_config_lines:
-                    formatted_output = "\n".join(running_config_lines)
-                    formatted_output = re.sub(r"WORKING", r"<span style='color:green;'>WORKING</span>", formatted_output)
-                    formatted_output = re.sub(r"CERTIFIED", r"<span style='color:red;'>CERTIFIED</span>", formatted_output)
-                    running_directory_outputs += f"{row['IP']}:\n{formatted_output}\n\n"
-
-        except Exception as e:
-            print(f"Connection failed for {row['IP']}: {e}")
+    while not output_queue.empty():
+        running_directory_outputs += output_queue.get()
 
     running_directory_outputs += "</pre></body></html>"
 
@@ -48,5 +59,6 @@ def execute_commands_and_save_to_html(template_path):
         with open(filepath, "w") as file:
             file.write(running_directory_outputs)
 
-template_path = 'template.xlsx'  # 替换为您模板文件的实际路径
-execute_commands_and_save_to_html(template_path)
+if __name__ == '__main__':
+    template_path = 'template.xlsx'  # 替换为您模板文件的实际路径
+    execute_commands_and_save_to_html(template_path)
